ad77afce5b5a1f441600f6bfe3e9ef42512893b2
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-10-10 22:16:49 -0300

    implement IMC calc

--- src/components/CInput.svelte
+++ src/components/CInput.svelte
@@ -1,12 +1,27 @@
 <script>
+  // to dispacth a event
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
   // use export to declare component props
+  export let label
   export let placeholder
   export let id
   export let type = 'text'
+
+  let value = null
+
+  const handleChange = event => {
+    const innerValue = event.target.value
+    value = innerValue
+
+    dispatch('value', Number(innerValue))
+  }
 </script>
 
 <div class="field">
-  <label class="label" for={id}>{placeholder}</label>
+  <label class="label" for={id}>{label}</label>
   <div class="control">
     <input
       class="input is-primary"
@@ -14,6 +29,8 @@
       min={1}
       {id}
       {placeholder}
+      {value}
+      on:change={handleChange}
     />
   </div>
 </div>
--- src/components/CMain.svelte
+++ src/components/CMain.svelte
@@ -1,27 +1,46 @@
 <script>
   import CInput from './CInput.svelte'
   import CMessage from './CMessage.svelte'
+  import { formatNumber } from '../utils'
 
   let imc = 0
+  let height = 0
+  let weight = 0
+
+  const setHeight = ({ detail }) => {
+    height = detail
+  }
+
+  const setWeight = ({ detail }) => {
+    weight = detail
+  }
+
+  const handleClick = () => {
+    imc = weight / (height * height)
+  }
 </script>
 
 <main class="section">
   <CInput
     id="weight"
+    label="Weight"
     placeholder="Weight"
+    on:value={setWeight}
   />
 
   <CInput
     id="height"
+    label="Height"
     placeholder="Height"
+    on:value={setHeight}
   />
 
-  <button class="button is-info"> Compute </button>
+  <button class="button is-info" on:click={handleClick}> Compute </button>
 
   <!-- conditionals -->
   {#if imc > 0}
     <hr />
 
-    <CMessage />
+    <CMessage {imc} />
   {/if}
 </main>
